af448c991a6b5cb83b23e819c4f9925c9bdca304
aungzawpyae <aungzawpyaeinf1001mm@gmail.com>
2022-01-06 09:09:56 +0630

    test

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,32 +5,60 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\ProductResource;
+use App\Models\Category;
+use App\Http\Resources\CollectionResource;
+use App\Models\Banner;
 use App\Models\Product;
+use App\Models\ProductCollection;
 
 class ProductController extends Controller
 {
     public function index(Request $request)
-    {
-        $name = $request->input('name');
-        $category_id = $request->input('category_id');
-        $perPage = $request->input('perPage');
+        {
+            $name = $request->input('name');
+            $category_id = $request->input('category_id');
+            $perPage = $request->input('perPage');
 
-        $products = Product::when($name, function($q) use($name){
-                        $q->where('name', 'ilike', '%'.$name.'%');
-                    })
-                    ->when($category_id, function($q) use($category_id){
-                        $q->where('category_id', $category_id);
-                    })
-                    ->latest()
-                    ->paginate($perPage);
-        
-        return ProductResource::collection($products);
-    }
+            $products = Product::when($name, function($q) use($name){
+                            $q->where('name', 'ilike', '%'.$name.'%');
+                        })
+                        ->when($category_id, function($q) use($category_id){
+                            $q->where('category_id', $category_id);
+                        })
+                        ->latest()
+                        ->paginate($perPage);
+            
+            return ProductResource::collection($products);
+        }
 
-     public function show(Request $request, $id)
-    {
-        $product = Product::find($id);
+
+    public function productAll(){
+
+           $product = Product::all();
+            return ProductResource::collection($product);
+        }
+        public function banner(){
         
-        return new ProductResource($product);
-    }
+            $product = Banner::all();
+             return ProductResource::collection($product);
+         }
+
+         public function collection(){
+            $product = Product::all();
+            $banner = Banner::all();
+            
+        //    $collection = ProductCollection::orderBy('id' ,'desc') ->limit(3)->get();
+           return ProductCollection::orderBy('id' ,'desc') ->limit(3)->get();
+         }
+
+         public function  collectionShow($id){
+
+            $data = Product::with('productCollection')->find($id);
+
+            $data = ProductCollection::with('products')->find($id);
+
+            return new CollectionResource($data);
+         }
+      
+    
 }
